b4a4dfa3613e07694bb8e11d97936d2a0785c9a9
Charles Menguy <menguy.charles@gmail.com>
2016-02-08 09:55:45 -0500

    Switching to CNAME for SITEURL

--- content/crime-analytics-sf-seattle.md
+++
@@ -1,3 +0,0 @@
-Title: Analyzing 2 months of real crime data from San Francisco and Seattle
-Date: 2016-02-05 12:35
-Tags: python, pandas, gis, dataviz, data-science
--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-#SITEURL = 'http://gradientdissent.com'
-SITEURL = "http://cmenguy.github.io"
+SITEURL = 'http://gradientdissent.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
